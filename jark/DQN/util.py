ce0c31295f51c15b477d73e1b2845ba85eca7685
notkamonohasi <notkamonohasi@gmail.com>
2023-05-13 13:38:41 +0900

    スペルミスしてた...

--- jark/DQN/util.py
+++
@@ -1,6 +0,0 @@
-from collections import namedtuple
-import torch
-
-Transition = namedtuple('Transition', 
-                        ('state', 'action', 'next_state', 'reward'))
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
--- jark/lane.py
+++
@@ -1,7 +0,0 @@
-
-from typing import Union
-
-class Lane : 
-    def __init__(self, init_data : dict[str, Union[int, float]]) -> None:
-        self.number = init_data["number"]
-        self.length = init_data["length"]

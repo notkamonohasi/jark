c58175db3ec4d4e45cde36fa8e30a4d59b6739e4
notkamonohasi <notkamonohasi@gmail.com>
2023-05-22 20:23:34 +0900

    エピソード毎に異なるシナリオを実行

--- jerk/main.py
+++ jerk/main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__" :
         "state_columns" : ["accel", "velocity", "distance_intersection", "BLUE", "YELLOW_TO_RED", "RED", "YELLOW_TO_BLUE", "remain_time"], 
         "learning_rate" : 0.0001, 
         "target_learning_rate" : 0.005, 
-        "buffer_size" : 10000, 
+        "buffer_size" : 20000, 
         "jerk_cand" : [-2, 0, 2],
         "batch_size" : 128,
         "gamma" : 0.995, 
@@ -20,67 +20,8 @@ if __name__ == "__main__" :
         "limit_velocity" : 20, 
         "limit_accel" : 2, 
         "limit_brake" : -3, 
-        "limit_step_count" : 300
+        "limit_step_count" : 1000
     }
-
-    # signal
-    signal_init_data_list = [
-        {
-            "number" : 0, 
-            "first_time" : random.randint(0, 1000), 
-            "interval_list" : [30, 4, 30, 4]
-        }
-    ]
-    init_data["signal_init_data_list"] = signal_init_data_list
-
-    # intersection
-    intersection_init_data_list = [
-        {
-            "number" : 0, 
-            "y" : 0, 
-            "x" : 0, 
-            "signal_number" : None
-        }, 
-        {
-            "number" : 1, 
-            "y" : 0, 
-            "x" : 400, 
-            "signal_number" : 0
-        }
-    ]
-    init_data["intersection_init_data_list"] = intersection_init_data_list
-
-    # lane
-    lane_init_data_list = []
-    for lane_number in range(1) : 
-        lane_init_data = {
-            "number" : lane_number, 
-            "from_intersection_number" : 0, 
-            "to_intersection_number" : 1
-        }
-        lane_init_data_list.append(lane_init_data)
-    init_data["lane_init_data_list"] = lane_init_data_list
-
-    # vehicle
-    vehicle_init_data_list = []
-    for vehicle_number in range(1) : 
-        vehicle_init_data = {
-            "number" : vehicle_number, 
-            "length" : 4.4, 
-            "decide_action_way" : "DQN", 
-            "velocity" : 2, 
-            "accel" : 0, 
-            "jerk" : 0, 
-            "lane_number" : 0, 
-            "lane_place" : 10 * 2 * vehicle_number,   # 適当
-            "route_list" : [0], 
-            "jerk_cand" : init_data["jerk_cand"], 
-            "limit_velocity" : init_data["limit_velocity"], 
-            "limit_accel" : init_data["limit_accel"], 
-            "limit_brake" : init_data["limit_brake"]
-        }
-        vehicle_init_data_list.append(vehicle_init_data)
-    init_data["vehicle_init_data_list"] = vehicle_init_data_list
 
     # dqnを初期化
     dqn = DQN({
@@ -98,6 +39,66 @@ if __name__ == "__main__" :
 
     for pos_episode in range(1, init_data["max_episode"] + 1) : 
         print(pos_episode)
+
+        # signal
+        signal_init_data_list = [
+            {
+                "number" : 0, 
+                "first_time" : random.randint(0, 1000), 
+                "interval_list" : [30, 4, 30, 4]
+            }
+        ]
+        init_data["signal_init_data_list"] = signal_init_data_list
+
+        # intersection
+        intersection_init_data_list = [
+            {
+                "number" : 0, 
+                "y" : 0, 
+                "x" : 0, 
+                "signal_number" : None
+            }, 
+            {
+                "number" : 1, 
+                "y" : 0, 
+                "x" : random.randint(300, 500), 
+                "signal_number" : 0
+            }
+        ]
+        init_data["intersection_init_data_list"] = intersection_init_data_list
+
+        # lane
+        lane_init_data_list = []
+        for lane_number in range(1) : 
+            lane_init_data = {
+                "number" : lane_number, 
+                "from_intersection_number" : 0, 
+                "to_intersection_number" : 1
+            }
+            lane_init_data_list.append(lane_init_data)
+        init_data["lane_init_data_list"] = lane_init_data_list
+
+        # vehicle
+        vehicle_init_data_list = []
+        for vehicle_number in range(1) : 
+            vehicle_init_data = {
+                "number" : vehicle_number, 
+                "length" : 4.4, 
+                "decide_action_way" : "DQN", 
+                "velocity" : 2, 
+                "accel" : 0, 
+                "jerk" : 0, 
+                "lane_number" : 0, 
+                "lane_place" : 10 * 2 * vehicle_number,   # 適当
+                "route_list" : [0], 
+                "jerk_cand" : init_data["jerk_cand"], 
+                "limit_velocity" : init_data["limit_velocity"], 
+                "limit_accel" : init_data["limit_accel"], 
+                "limit_brake" : init_data["limit_brake"]
+            }
+            vehicle_init_data_list.append(vehicle_init_data)
+        init_data["vehicle_init_data_list"] = vehicle_init_data_list
+
         init_data["pos_episode"] = pos_episode
         init_data["result_path"] = "./result/episode_" + str(pos_episode).zfill(4)
         dqn.pos_episode = pos_episode
